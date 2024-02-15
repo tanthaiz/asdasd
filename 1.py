3b9de982de5fd348c8b20b4841c8c5932e7a55db
tanthaiz <139226043+tanthaiz@users.noreply.github.com>
2024-02-15 15:37:35 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+num = 1
+while num <= 21:
+    print(num, end=",")
+    num += 2
+num = 2
+while num <= 17:
+    print(num, end=",")
+    num += 3
+num = 100
+while num >= -90:
+    print(num, end=",")
+    num -= 10
+num = -30
+while num <= 30:
+    print(num, end=",")
+    num += 10
+num = 15
+while num <= 55:
+    print(num, end=",")
+    num += 8
+    
